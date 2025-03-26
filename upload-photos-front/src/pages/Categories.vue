ea420d1d8205c762056198a2452182d43b68dab2
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-26 12:09:00 +0200

    Small change

--- upload-photos-front/src/pages/Categories.vue
+++ upload-photos-front/src/pages/Categories.vue
@@ -44,7 +44,6 @@ onMounted(() => {
               </th>
               <td class="px-6 py-4 text-right flex justify-end gap-4">
                 <button type="submit"
-
                         class="rounded-md bg-indigo-600 px-3 py-1 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline  focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   Edit
                 </button>

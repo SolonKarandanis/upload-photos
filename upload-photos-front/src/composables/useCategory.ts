4bbd874b73a6c8f528c0065234a4d819367cbc2d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-26 11:40:51 +0200

    Created useCategory composable

---
+++ upload-photos-front/src/composables/useCategory.ts
@@ -0,0 +1,60 @@
+import {ref} from "vue";
+import type {Category} from "../models/category.model.ts";
+import axiosClient from "../axios.ts";
+import router from "../router.ts";
+import type {AxiosResponse} from "axios";
+
+export function useCategory(){
+    const iLoading = ref(false);
+    const errorMessage = ref('');
+    const categories = ref<Category[]>([]);
+
+    const createCategory=(values:{name: string})=>{
+        iLoading.value=true;
+        axiosClient.get('/sanctum/csrf-cookie').then(()=>{
+            axiosClient.post("/api/categories",values)
+                .then(() => {
+                    iLoading.value=false;
+                    router.push({name: 'Categories'})
+                })
+                .catch(error => {
+                    iLoading.value=false;
+                    console.log(error.response)
+                    errorMessage.value = error.response.data.message;
+                })
+        });
+    }
+
+    const fetchCategories=()=>{
+        iLoading.value=true;
+        axiosClient.get('/api/categories')
+            .then((response:AxiosResponse<Category[]>) => {
+                iLoading.value=false;
+                categories.value = response.data;
+            }).catch(error => {
+                iLoading.value=false;
+                console.log(error.response)
+            })
+    }
+
+    const deleteCategory = (categoryId:number)=>{
+        iLoading.value=true;
+        axiosClient.delete(`/api/categories/${categoryId}`)
+            .then(() => {
+                iLoading.value=false;
+                categories.value = categories.value.filter(category => category.id !== categoryId)
+            }).catch(error => {
+                iLoading.value=false;
+                console.log(error.response)
+            })
+    }
+
+    return {
+        iLoading,
+        categories,
+        errorMessage,
+        createCategory,
+        fetchCategories,
+        deleteCategory,
+    }
+}

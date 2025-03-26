364ee74b57051b2601fb475aebef4d87b1c33fd9
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-26 11:56:58 +0200

    Fetching categories

--- upload-photos-back/app/Http/Requests/Categories/UpdateCategoryRequest.php
+++ upload-photos-back/app/Http/Requests/Categories/UpdateCategoryRequest.php
@@ -22,7 +22,7 @@ class UpdateCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required|unique:categories', 'string', 'max:50'],
+            'name' => ['required', 'string', 'max:50','unique:categories'],
         ];
     }
 }
--- upload-photos-front/src/pages/Categories.vue
+++ upload-photos-front/src/pages/Categories.vue
@@ -1,12 +1,49 @@
 <script setup lang="ts">
 
 import Header from "../components/Header.vue";
+import {useCategory} from "../composables/useCategory.ts";
+import {onMounted} from "vue";
+
+const {fetchCategories,categories,iLoading} = useCategory();
+
+onMounted(() => {
+  fetchCategories();
+})
 </script>
 
 <template>
   <Header>
     Categories
   </Header>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" class="px-6 py-3">
+                <span class="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr  v-for="category in categories" :key="category.id"
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{category.name}}
+              </th>
+              <td class="px-6 py-4 text-right">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>

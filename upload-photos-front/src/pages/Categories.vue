5a3cf371fef8ee0803347b5300ff23bde898c05c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-26 12:10:11 +0200

    Small changes

--- upload-photos-front/src/pages/Categories.vue
+++ upload-photos-front/src/pages/Categories.vue
@@ -26,13 +26,13 @@ onMounted(() => {
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <thead class="text-xs text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" class="px-6 py-3">
-                Product name
+                Category Name
               </th>
               <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
+                <span class="sr-only">Actions</span>
               </th>
             </tr>
           </thead>

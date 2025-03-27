37c0247f2d3abb35fc99d0aac1171dc3317615d1
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-27 17:12:08 +0200

    Added Vue Toast Notification

--- upload-photos-front/src/main.ts
+++ upload-photos-front/src/main.ts
@@ -3,10 +3,13 @@ import './style.css'
 import App from './App.vue'
 import router from "./router.ts";
 import {createPinia} from "pinia";
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 const pinia = createPinia()
 
 createApp(App)
     .use(router)
     .use(pinia)
+    .use(ToastPlugin)
     .mount('#app')

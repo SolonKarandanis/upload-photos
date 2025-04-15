7baf69d734ea35349ea2a5015d15311572409abc
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-04-15 18:03:40 +0300

    Changes

--- upload-photos-back/_ide_helper_models.php
+++ upload-photos-back/_ide_helper_models.php
@@ -16,6 +16,31 @@ namespace App\Models{
  * 
  *
  * @property int $id
+ * @property int|null $user_id
+ * @property string $account_number
+ * @property string $balance
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account whereAccountNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account whereBalance($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Account whereUserId($value)
+ */
+	class Account extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
  * @property string $name
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
@@ -67,7 +92,7 @@ namespace App\Models{
  * @property int $created_by
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Categories> $categories
  * @property-read int|null $categories_count
- * @property-read \App\Models\User|null $createdBy
+ * @property-read \App\Models\User $createdBy
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Posts newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Posts newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Posts query()
@@ -88,6 +113,80 @@ namespace App\Models{
  * 
  *
  * @property int $id
+ * @property string $reference
+ * @property int $user_id
+ * @property int $account_id
+ * @property int|null $transfer_id
+ * @property string $amount
+ * @property string|null $balance
+ * @property string $category
+ * @property int $confirmed
+ * @property string|null $description
+ * @property string $date
+ * @property string|null $meta
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereAccountId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereBalance($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereConfirmed($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereMeta($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereReference($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereTransferId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereUserId($value)
+ */
+	class Transaction extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int|null $sender_id
+ * @property int|null $sender_account_id
+ * @property int|null $recipient_id
+ * @property int|null $recipient_account_id
+ * @property string $reference
+ * @property string $status
+ * @property string $amount
+ * @property string|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereRecipientAccountId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereRecipientId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereReference($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereSenderAccountId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereSenderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transfer whereUpdatedAt($value)
+ */
+	class Transfer extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
  * @property string $name
  * @property string $email
  * @property \Illuminate\Support\Carbon|null $email_verified_at
@@ -95,6 +194,7 @@ namespace App\Models{
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $phone_number
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Posts> $posts
@@ -109,6 +209,7 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePhoneNumber($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
  */

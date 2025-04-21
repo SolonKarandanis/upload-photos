25add81f23e6d78f32523bff40cd0e7eb2e10995
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-04-21 09:40:38 +0300

    Changes

---
+++ upload-photos-back/app/Http/Requests/Auth/RegisterUserRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:2', 'max:200'],
+            'email' => ['required', 'email', 'max:200', 'unique:users'],
+            'password' => ['required', 'string', 'max:200'],
+            'phone_number' => ['required', 'string', 'min:10', 'max:20', 'unique:users']
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}

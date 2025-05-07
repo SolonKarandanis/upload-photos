fed8b4c6d4e493e531a0aaee4534ec8dabfb1fed
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-07 14:22:15 +0300

    Added method in UserRepo

--- upload-photos-back/app/Repositories/UserRepository.php
+++ upload-photos-back/app/Repositories/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Dtos\UserDto;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class UserRepository implements UserRepositoryInterface
@@ -23,5 +24,15 @@ class UserRepository implements UserRepositoryInterface
     public function getUserById(int $userId): Builder|User
     {
         return User::query()->where('id', $userId)->first();
+    }
+
+    public function getUsersWithPostCounts(): Builder|Collection
+    {
+        return User::query()->withCount([
+            'posts',
+            'posts as laravel_posts_count' => function (Builder $query) {
+                $query->where('title', 'LIKE', '%laravel_posts_count%');
+            }
+        ])->get();
     }
 }
--- upload-photos-back/app/Repositories/UserRepositoryInterface.php
+++ upload-photos-back/app/Repositories/UserRepositoryInterface.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Dtos\UserDto;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 interface UserRepositoryInterface
@@ -12,4 +13,6 @@ interface UserRepositoryInterface
 
     public function  createUser(UserDto  $userDto): Builder |User;
     public function  getUserById(int $userId): Builder|User;
+
+    public function getUsersWithPostCounts():Builder| Collection;
 }

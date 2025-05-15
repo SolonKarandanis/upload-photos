28c7f48e03c933b4ee32de9f103551675ce486e6
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-15 14:24:45 +0300

    Added soft deletes in Images

--- upload-photos-back/app/Models/Image.php
+++ upload-photos-back/app/Models/Image.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
+    use SoftDeletes, Prunable;
     protected $fillable = ['path', 'label'];
+
+    public function prunable()
+    {
+        // Files matching this query will be pruned
+        return static::query()->where('deleted_at', '<=', now()->subDays(14));
+    }
+
+    protected function pruning():void
+    {
+        // Remove the file from s3 before deleting the model
+        Storage::disk('s3')->delete($this->filename);
+    }
 }
+
+// Add PruneCommand to your schedule (app/Console/Kernel.php)
+//$schedule->command(PruneCommand::class)->daily();

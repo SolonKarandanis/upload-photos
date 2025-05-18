67810c8a003dcf27e203702b05cfd8624b5b8834
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-18 10:11:37 +0300

    Small change

--- upload-photos-back/app/Models/User.php
+++ upload-photos-back/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -72,5 +73,15 @@ class User extends Authenticatable
     public function account(): HasOne
     {
         return $this->hasOne(Account::class, 'user_id');
+    }
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
+    }
+
+    public function favouriteCars():BelongsToMany
+    {
+        return $this->belongsToMany(Car::class,'favourite_cars','user_id','car_id');
     }
 }

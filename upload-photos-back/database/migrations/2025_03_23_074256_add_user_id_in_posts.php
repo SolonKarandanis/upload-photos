cc46223447d725328dc40663de30bcaf5670023a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-28 11:02:26 +0300

    Added WithDatabaseDriver

--- upload-photos-back/database/migrations/2025_03_23_074256_add_user_id_in_posts.php
+++ upload-photos-back/database/migrations/2025_03_23_074256_add_user_id_in_posts.php
@@ -1,19 +1,29 @@
 <?php
 
+use App\Traits\WithDatabaseDriver;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    use WithDatabaseDriver;
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->foreignId('created_by')
-                ->index()->constrained('users');
+            $column = $table->foreignId('created_by')
+                ->index();
+            // This is where we apply the "fix" if
+            // the driver is SQLite
+            if ($this->databaseDriverIs("sqlite")) {
+                $column->nullable()->constrained('users');
+            }
+            else{
+                $column->constrained('users');
+            }
         });
     }
 
